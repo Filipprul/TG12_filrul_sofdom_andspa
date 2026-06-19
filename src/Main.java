ae2a0eaca59d8c40e357c617001c0f5ec7cbd295
Filipp <filipprul2007@gmail.com>
2026-06-19 16:41:11 +0200

    Menü V0.0.1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import menu.menu;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Snake Game");
+
+        menu gamemenu = new menu();
+
+        Scene menuScene = gameMenu.getMenuScene(primaryStage);
+
+        primaryStage.setScene(menuScene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args)
+    }
+}
--- src/menu/menu.java
+++ src/menu/menu.java
@@ -1,5 +1,13 @@
 package menu;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 public class menu{
     public Scene getMenuScene(Stage primaryStage){
         VBox root = new VBox(25);
@@ -12,5 +20,35 @@ public class menu{
 
         Button startButton = new Button("START");
         Button exitButton = new Button("EXIT");
+
+        String buttonStyle = "-fx-background-color: #333333; " +
+        "-fx-text-fill: white; " +
+        "-fx-font-size: 16px; " +
+        "-fx-font-weight: bold; " +
+        "-fx-min-width: 200px; " +
+        "-fx-min-height: 45px; " +
+        "-fx-background-radius: 5px; " +
+        "-fx-cursor: hand; ";
+
+        startButton.setStyle(buttonStyle);
+        exitButton.setStyle(buttonStyle);
+
+        startButton.setOnMouseEntered(e -> startButton.setStyle(buttonStyle + "-fx-background-color: #4CAF50;"));
+        startButton.setOnMouseExited(e -> startButton.setStyle(buttonStyle));
+
+        exitButton.setOnMouseEntered(e -> exitButton.setStyle(buttonStyle + "-fx-background-color: #f44336;"));
+        exitButton.setOnMouseExited(e -> exitButton.setStyle(buttonStyle));
+
+        startButton.setOnAction(e -> {
+            System.out.println("Spiel startet...");
+        })
+
+        exitButton.setOnAction(e -> {
+            primaryStage.close();
+        })
+
+        root.getChildren().addAll(titleLabel, startButton, exitButton);
+
+        retunr new Scene(root, 800, 600);
     }
 }
