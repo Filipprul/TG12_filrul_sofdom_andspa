23875c07700d87f6e80d2de4ef157a09c1723462
Filipp <filipprul2007@gmail.com>
2026-06-28 20:32:40 +0200

    Refactor Main class to initialize GUI or terminal menu based on startGUI flag

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+package src;
+
+import view.gui.GUI_SNAKE_Portal;
+import view.terminal.TerminalMenu;
+
 public class Main {
     public static void main(String[] args) {
-        menu.menu hauptmenu = new menu.menu();
-        hauptmenu.zeigeMenue();
+        boolean startGUI = true;
+
+        if (startGUI) {
+            // Hier kommt der Code für die GUI-Initialisierung.
+            GUI_SNAKE_Portal gui = new GUI_SNAKE_Portal();
+        } else {
+            // Nicht fertige Terminal-Implementierung.
+            TerminalMenu menu = new TerminalMenu();
+            menu.zeigeMenue();
+        }
     }
 }
