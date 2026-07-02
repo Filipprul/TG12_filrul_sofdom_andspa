86520dbab59f94709556aead724d7b0fcc4c5537
Sofia <domkesofia@gmail.com>
2026-07-02 08:51:18 +0200

    Update Main class to set startGUI to false and adjust GUI_SNAKE_Portal instantiation; add VSCode settings file for project configuration

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-package src;
-
-import view.gui.GUI_SNAKE_Portal;
-import view.terminal.TerminalMenu;
+import view.gui.*;
+import view.terminal.*;
 
 public class Main {
     public static void main(String[] args) {
-        boolean startGUI = true;
+        boolean startGUI = false;
 
         if (startGUI) {
             // Hier kommt der Code für die GUI-Initialisierung.
-            GUI_SNAKE_Portal gui = new GUI_SNAKE_Portal();
+            GUI_SNAKE_Portal Gui = new GUI_SNAKE_Portal();
         } else {
             // Nicht fertige Terminal-Implementierung.
             TerminalMenu menu = new TerminalMenu();
