068001d9bb35631744e7a4d7dfa4c88871174797
AndyArne <andreas.spangenberg@aol.com>
2026-07-10 09:16:03 +0200

    Habe ein bischen in der gui gearbeited
    die gui sollte jetzt auch essen und den rest der schlange zeichnen.
    am aussehen kann vieleicht aber noch gearbeited werden.

--- src/view/gui/gui.java
+++ src/view/gui/gui.java
@@ -18,18 +18,32 @@ public class gui extends PApplet {
         drawGrid(first, second, 47, 18, 18);
         drawSnake();
     }
-
     void drawSnake() {
         fill(255);
-        circle(34, 34, 45);
+        circle(Snake[0].get_x * 47 + 34, Snake[0].get_y * 47 + 34, 45);
+        for(int i=1; i < Snake.size; i++){
+            fill(205,127,50);
+            circle(Snake[i].get_x* 47 + 34, Snake[i].get_y * 47 + 34, 45);
+        }
     }
 
+    void drawFood(){
+        for(int py = 0; py < grid_size.size; py++){
+            for(int px = 0 < grid_size[0].size; px++){
+                if(grid_size[py][px].get_value() == 2){
+                    fill(129,0,21);
+                    circle(px * 47 + 34, py * 47 + 34, 30)
+                }
+            }
+        }
+    }
+    
     // renamed from draw to avoid conflict with Processing's draw()
     void drawGrid(int firstColor, int secondColor, int size, int nx, int ny) {
         for (int y = 0; y < ny; y++) {
             for (int x = 0; x < nx; x++) {
                 farbwechsel(firstColor, secondColor);
-                rect(10 + x*size, 10 + y*size, size, size);
+                rect(10 + x * size, 10 + y * size, size, size);
             }
             farbwechsel(firstColor, secondColor);
         }
