d233f5aa3ddcd2de4b362798c57ee037f386db93
Filipp <filipprul2007@gmail.com>
2026-07-15 17:44:11 +0200

    Refactor Main class to streamline state management and remove unused classes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,51 +1,66 @@
 package main;
 import processing.core.PApplet;
-import view.gui.gui;
+import view.game.game;
 import view.login.login;
-import view.menu.highscoreAnzeige;
+import view.menu.menu;
+import view.gameover.gameover;
 
 public class Main extends PApplet {
-    enum State { LOGIN, HIGHSCORE, GAME, MENU}
+    public enum State { LOGIN, MENU, GAME, REGISTER, GAMEOVER}
     State currentState = State.LOGIN;
 
     login loginScreen;
-    gui game;
-    highscoreAnzeige highscoreScreen;
+    game gameScreen;
+    menu menuScreen;
+    gameover gameoverScreen;
 
-    public void settings() {size(1000, 1000);
-    }
+    public void settings() {size(1000, 1000);}
 
     public void setup() {
         loginScreen = new login(this);
-        game = new gui(this);
-        highscoreScreen = new highscoreAnzeige(this);
+        gameScreen = new game(this);
+        menuScreen = new menu(this);
+        gameoverScreen = new gameover(this);
     }
 
     public void draw() {
-        if (currentState == State.LOGIN) {
-            loginScreen.draw();
-            if (loginScreen.isLoggedIn()) {
-                currentState = State.HIGHSCORE;
-            }
-        } else if (currentState == State.HIGHSCORE) {
-            highscoreScreen.draw();
-            if (highscoreScreen.ishighscoreVisible()) {
-                currentState = State.GAME;
-            }
-        } else {
-            game.draw();
+        switch (currentState) {
+            case LOGIN:
+                loginScreen.draw();
+                if (loginScreen.isLoggedIn()) currentState = State.MENU;
+                break;
+            case MENU:
+                menuScreen.draw();
+                if (menuScreen.ishighscoreVisible()){
+                    currentState = State.GAME;
+                    menuScreen.resetMenu();
+                }
+                break;
+            case GAME:
+                gameScreen.draw();
+                break;
+            case GAMEOVER:
+                gameoverScreen.over(gameScreen.getController().getGrid().getScore());
+                break;
         }
     }
 
     public void keyPressed() {
-        if (currentState == State.LOGIN) {
-            loginScreen.keyPressed(key);
-        } else if (currentState == State.HIGHSCORE) {
-            highscoreScreen.keyPressed(key);
-        } else {
-            game.keyPressed(key);
+        if (currentState == State.GAMEOVER){
+            if(key == 'r' || key == 'R'){
+                gameScreen.resetGame();
+                currentState = State.GAME;
+            } else if (key == 'm' || key == 'M') {
+                gameScreen.resetGame();
+                currentState = State.MENU;}
         }
+
+        if (currentState == State.LOGIN) {loginScreen.keyPressed(key);
+        } else if (currentState == State.MENU) {menuScreen.keyPressed(key);
+        } else {gameScreen.keyPressed(key);}
     }
+
+    public void setState(State state){currentState = state;}
 
     public static void main(String[] args) {PApplet.main("main.Main");}
 }
