d137cdd97c0559d0efef4a4940539a5ccdbc781b
Filipp <filipprul2007@gmail.com>
2026-07-16 20:41:51 +0200

    Add constants for game configuration and database connection

--- src/game/core/Constants.java
+++ src/game/core/Constants.java
@@ -1,4 +1,14 @@
 package game.core;
 
-public class Constants {
+public final class Constants {
+    // Spielfeld
+    public static final int GRID_SIZE = 18;
+    public static final int CELL_SIZE = 47;
+    public static final int SCREEN_WIDTH = 1000;
+    public static final int SCREEN_HEIGHT = 1000;
+
+    // Datenbank (Hier später besser über .env laden)
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/snake_game";
+    public static final String DB_USER = "root";
+    public static final String DB_PASS = "IamFilipp&Rul";
 }
