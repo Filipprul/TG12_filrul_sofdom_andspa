393a7406e6e28f4d948103e736bed5b40ead97d7
Sofia <domkesofia@gmail.com>
2026-07-17 08:31:36 +0200

    Remove DB password, add docs, update IDE config
    Removed the hard-coded DB password in src/game/core/Constants.java (DB_PASS set to empty) to avoid committing credentials. Added two handout documents to docs/ (including a temporary ~$ file). Updated .idea/misc.xml languageLevel from JDK_X to JDK_25. Minor EOF/formatting change in Constants.java.

--- src/game/core/Constants.java
+++ src/game/core/Constants.java
@@ -3,5 +3,5 @@ package game.core;
 public final class Constants {
     public static final String DB_URL = "jdbc:mysql://localhost:3306/snake_game";
     public static final String DB_USER = "root";
-    public static final String DB_PASS = "IamFilipp&Rul";
+    public static final String DB_PASS = "";
 }
